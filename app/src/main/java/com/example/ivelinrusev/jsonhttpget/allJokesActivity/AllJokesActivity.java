b5b3ee06f46283b0819a8c62b01dd07ffa6b0ea7
Ivelin Rusev <optodent93@gmail.com>
2015-06-29 16:39:54 +0300

    Context Toolbar fixed, remove item from db impl

--- app/src/main/java/com/example/ivelinrusev/jsonhttpget/allJokesActivity/AllJokesActivity.java
+++ app/src/main/java/com/example/ivelinrusev/jsonhttpget/allJokesActivity/AllJokesActivity.java
@@ -1,8 +1,10 @@
 package com.example.ivelinrusev.jsonhttpget.allJokesActivity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,9 +17,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.ivelinrusev.jsonhttpget.R;
 import com.example.ivelinrusev.jsonhttpget.dataBaseComponents.JokeDB;
+import com.example.ivelinrusev.jsonhttpget.dataBaseComponents.JokesDataSource;
 import com.example.ivelinrusev.jsonhttpget.mainActivity.MainActivity;
 
 import java.util.ArrayList;
@@ -30,6 +34,11 @@ public class AllJokesActivity extends ActionBarActivity {
     private EditText inputSearch;
     private Toolbar toolbar;
     private ActionMode actionMode;
+    private JokesDataSource dataSource;
+    private ArrayList<JokeDB> list;
+    private ArrayList<View> checkedViews = new ArrayList<View>();
+    private ArrayList<String> checkedViewsItems = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +47,34 @@ public class AllJokesActivity extends ActionBarActivity {
         setContentView(R.layout.activity_all_jokes);
         listView = (ListView)findViewById(R.id.list_item);
         inputSearch = (EditText)findViewById(R.id.input_search);
-        ArrayList<JokeDB> list = (ArrayList<JokeDB>)getIntent().getSerializableExtra("list");
-        theAdapter = new ArrayAdapter<String>(this ,R.layout.text_view, jokesToStringArray(list));
+        list = (ArrayList<JokeDB>)getIntent().getSerializableExtra("list");
 
+        theAdapter = new ArrayAdapter<String>(this ,R.layout.text_view, jokesToStringArray(list));
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         listView.setAdapter(theAdapter);
         listView.setLongClickable(true);
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
-        {
+        dataSource = new JokesDataSource(this);
+        dataSource.open();
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
             public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
 
+                //checks if checkedItems array doesn't contains the selected item, if so it is added.
+                if (!checkedViews.contains(arg1)) {
+                    checkedViews.add(arg1);
+                    checkedViewsItems.add(theAdapter.getItem(pos).toString());
+                }
+
                 MyActionModeCallBack callback = new MyActionModeCallBack();
-                toolbar.setVisibility(View.INVISIBLE);
                 actionMode = startActionMode(callback);
+                toggle(arg1, pos, theAdapter.getItem(pos).toString());
                 return true;
 
             }
         });
 
-
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         inputSearch.addTextChangedListener(new TextWatcher() {
@@ -103,28 +119,59 @@ public class AllJokesActivity extends ActionBarActivity {
             return true;
         }
 
-        if(id == R.id.navigate){
+        else if(id == R.id.navigate){
           Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private String[] jokesToStringArray(ArrayList<JokeDB> list){
+    private ArrayList<String> jokesToStringArray(ArrayList<JokeDB> list){
 
         Iterator iterator = list.iterator();
-        String[] jokes = new String[list.size()];
-        int index = 0;
+        ArrayList<String> jokes = new ArrayList<String>();
         while (iterator.hasNext()){
-            jokes[index++] = iterator.next().toString();
+            jokes.add(iterator.next().toString());
         }
         return jokes;
+    }
+
+    private void toggle(View view, int pos, String item){
+
+        if (listView.isItemChecked(pos)) {
+
+            checkedViews.remove(view);
+            checkedViewsItems.remove(item);
+            actionMode.setTitle(checkedViews.size() + " Selected");
+            listView.setItemChecked(pos, false);;
+            view.setBackgroundColor(Color.TRANSPARENT);
+            if (checkedViews.size() == 0) actionMode.finish();
+
+        } else {
+
+            listView.setItemChecked(pos, true);
+            actionMode.setTitle(checkedViews.size() + " Selected");
+            view.setBackgroundColor(Color.GRAY);
+
+        }
+    }
+
+    private  JokeDB getJokeByValue(String str){
+
+        for(JokeDB joke : list){
+            if (str.contains(joke.getJoke())){
+                return joke;
+            }
+        }
+        return null;
     }
 
     private class MyActionModeCallBack implements  ActionMode.Callback{
 
         @Override
         public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+
+            toolbar.setVisibility(View.GONE);
             mode.getMenuInflater().inflate(R.menu.context, menu);
             return true;
         }
@@ -136,8 +183,24 @@ public class AllJokesActivity extends ActionBarActivity {
 
         @Override
         public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            actionMode.getCustomView();
-            return true;
+
+            switch (item.getItemId()) {
+                case R.id.delete:
+
+                    for(int i = 0; i < checkedViews.size(); i++) {
+                        checkedViews.get(i).setBackgroundColor(Color.TRANSPARENT);
+                        theAdapter.remove(checkedViewsItems.get(i));
+                        dataSource.deleteJoke(getJokeByValue(checkedViewsItems.get(i)));
+                    }
+                    checkedViewsItems.clear();
+                    checkedViews.clear();
+                    theAdapter.notifyDataSetChanged();
+                    actionMode.finish();
+                    Toast.makeText(getBaseContext(), "Selected items removed", Toast.LENGTH_SHORT).show();
+                    return true;
+
+            }
+            return false;
         }
 
         @Override
